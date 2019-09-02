1f18b28111934b0814cdc51c2e814cef415a93a5
Junru Xiong <44308338+joeyhsiung@users.noreply.github.com>
2019-09-02 15:12:10 +0100

    Add files via upload

--- Simulation/run.py
+++ Simulation/run.py
@@ -3,9 +3,9 @@
 
 from Simulation import model
 
-model = model.AreaPerson(firm_file='D:/UCL/dissertation/tb/final_data/0821/initialize_data/firm.csv',
-                         citizen_file='D:/UCL/dissertation/tb/final_data/0821/initialize_data/citizen.csv',
-                         ward_file='D:/UCL/dissertation/tb/final_data/0821/initialize_data/ward.csv',
-                         map_file='D:/UCL/dissertation/tb/final_data/0821/地图文件/greater_london_wards.shp')
+model = model.AreaPerson(firm_file='initialize_data/firm.csv',
+                         citizen_file='initialize_data/citizen.csv',
+                         ward_file='initialize_data/ward.csv',
+                         map_file='greater_london_wards.shp')
 
 model.run_model()
